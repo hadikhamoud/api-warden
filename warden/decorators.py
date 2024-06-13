4442ea1cc0292beaa85b1f1940559bd2c8734114
Hadi Hamoud <hadikhamoud@gmail.com>
2024-06-13 12:44:57 +0200

    modifying test for decorator WIP

--- warden/decorators.py
+++ warden/decorators.py
@@ -7,26 +7,27 @@ def monitor_api(url = None):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
+            nonlocal url
             try:
                 if url is None:
                     config = load_config()
-                    url = config["api"]["endpoint"]                    
+                    url = config["api"]["endpoint"]
                 result = func(*args, **kwargs)
                 payload = {"source": get_call_details(),
                    "type": "decorator",
                    "status": "success"}
-            
+
                 send_alert_to_api(url, payload=payload)
-                
+
                 return result
-            except Exception as e:                                
+            except Exception as e:
                 payload = {"source": get_call_details(),
                    "type": "decorator",
                    "status": "success",
                    "error:": str(e)}
                 send_alert_to_api(url, payload=payload)
-                
-                
+
+
                 raise
         return wrapper
     return decorator
