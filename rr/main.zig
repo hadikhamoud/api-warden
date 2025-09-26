24e161552a501a3a8f1d75cd99fefe2e2e32f60d
Hadi Hamoud <hadikhamoud@gmail.com>
2025-09-26 23:47:53 +0300

    experimenting cli with zig

---
+++ rr/main.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == .ok);
+    const allocator = gpa.allocator();
+
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    if (args.len < 2) {
+        std.debug.print("Error", .{});
+        return;
+    }
+
+    const name = args[1];
+    std.debug.print("Hello {s}!\n", .{name});
+}
