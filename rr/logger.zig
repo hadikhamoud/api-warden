c205b99d9e0cdc873b72089bb619cd7c33684030
Hadi Hamoud <hadikhamoud@gmail.com>
2025-10-29 09:34:50 +0300

    using datetime.zig from Jarius Martin (will change later)

--- rr/logger.zig
+++ rr/logger.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const time = @import("datetime.zig");
 
 pub const std_options: std.Options = .{
     .log_level = .info,
@@ -12,17 +13,21 @@ pub fn structLogFn(
     args: anytype,
 ) void {
     const scope_prefix = "(" ++ switch (scope) {
-        .my_project, .nice_library, std.log.default_log_scope => @tagName(scope),
+        std.log.default_log_scope => @tagName(scope),
         else => if (@intFromEnum(level) <= @intFromEnum(std.log.Level.err))
             @tagName(scope)
         else
             return,
     } ++ "): ";
 
-    const prefix = "[" ++ comptime level.asText() ++ "] " ++ scope_prefix;
+    const prefix = "[" ++ comptime level.asText() ++ "] ";
 
     std.debug.lockStdErr();
     defer std.debug.unlockStdErr();
     var stderr = std.fs.File.stderr().writer(&.{});
-    nosuspend stderr.interface.print(prefix ++ format ++ "\n", args) catch return;
+    const timestamp = time.Datetime.now();
+    var buf: [32]u8 = undefined;
+    const timestamp_str = timestamp.formatHttpBuf(&buf) catch "INVALID_TIME";
+
+    nosuspend stderr.interface.print(prefix ++ "{s} " ++ scope_prefix ++ format ++ "\n", .{timestamp_str} ++ args) catch return;
 }
