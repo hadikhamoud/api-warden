fbb366dec6ef33608a0e0556d17a8190cc34ab15
Hadi Hamoud <hadikhamoud@gmail.com>
2025-11-10 01:00:55 +0300

    creating Webhook details and storing

--- rr/main.zig
+++ rr/main.zig
@@ -5,10 +5,24 @@ const logger = @import("logger.zig");
 
 pub const std_options = logger.std_options;
 
+const WebhookDetails = struct { url: []const u8, headers: []std.http.Header };
+
 fn startProcess(arguments: [][:0]u8, alloc: std.mem.Allocator) !i32 {
     var child = std.process.Child.init(arguments, alloc);
-    _ = try child.spawn();
+    _ = try child.spawnAndWait();
     return child.id;
+}
+
+pub fn writeWebhookDetails(alloc: std.mem.Allocator) !void {
+    const data_path = xdg.getDataPath(alloc);
+    const webhook_path = try std.fmt.allocPrint(alloc, "{s}/webhooks", .{});
+    defer alloc.free(data_path);
+    defer alloc.free(webhook_path);
+    const mode: u32 = 0o755;
+    if (std.posix.mkdir(webhook_path, mode)) |_| {} else |err| switch (err) {
+        error.PathAlreadyExists => {},
+        else => return err,
+    }
 }
 
 pub fn main() !void {
@@ -40,11 +54,44 @@ pub fn main() !void {
             .{ .name = "X-API-Key", .value = "" },
             .{ .name = "Content-Type", .value = "application/json" },
         };
+
         const url = args[2];
         const body = if (args.len > 3) args[3] else "";
         const response = try rest_methods.post(url, headers, body, allocator);
         defer allocator.free(response);
         std.log.info("{s}", .{response});
+    } else if (std.mem.eql(u8, cmd, "set-webhook")) {
+        if (args.len < 3) {
+            std.log.err("you didn't provide any arguments buddy", .{});
+            return;
+        }
+
+        const url = args[2];
+        const headers_str = if (args.len > 3) args[3] else "";
+        const headers_json = try std.json.parseFromSlice(std.json.Value, allocator, headers_str, .{});
+        defer headers_json.deinit();
+        var headers = try std.ArrayList(std.http.Header).initCapacity(allocator, 1024);
+        defer headers.deinit(allocator);
+        var it = headers_json.value.object.iterator();
+        while (it.next()) |entry| {
+            const name = entry.key_ptr.*;
+            const value = entry.value_ptr.*.string;
+            try headers.append(allocator, .{ .name = name, .value = value });
+        }
+
+        std.log.info("Parsed url {s}:\n", .{url});
+        std.log.info("Parsed headers:\n", .{});
+
+        for (headers.items) |h| {
+            std.log.info("{s}: {s}\n", .{ h.name, h.value });
+        }
+
+        const webhook_details = WebhookDetails{
+            .url = url,
+            .headers = headers.items,
+        };
+
+        std.log.info("WebhookDetails created with {d} headers\n", .{webhook_details.headers.len});
     } else {
         std.log.info("\nWrite a command", .{});
     }
